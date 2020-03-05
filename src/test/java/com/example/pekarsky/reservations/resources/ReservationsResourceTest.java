11245406e9af9895ad305ecb51ffd062809a35c9
serge.pekarsky@gmail.com <idlev100$>
2020-03-05 15:44:24 +0100

    some unit tests on resource

--- src/test/java/com/example/pekarsky/reservations/resources/ReservationsResourceTest.java
+++ src/test/java/com/example/pekarsky/reservations/resources/ReservationsResourceTest.java
@@ -1,6 +1,8 @@
 package com.example.pekarsky.reservations.resources;
 
+import com.example.pekarsky.reservations.dto.ReservationDto;
 import com.example.pekarsky.reservations.mapper.ReservationDtoMapper;
+import com.example.pekarsky.reservations.model.Reservation;
 import com.example.pekarsky.reservations.service.ReservationService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -8,17 +10,29 @@ import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
-import static org.mockito.Mockito.only;
-import static org.mockito.Mockito.verify;
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ValidationException;
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 class ReservationsResourceTest {
+
+    private static final String URL = "http://";
+    private static final long SAME_RESERVATION_ID = 1000L;
+    private static final long DIFFERENT_RESERVATION_ID = 9999L;
 
     @Mock
     private ReservationService reservationServiceMock;
 
     @Mock
     private ReservationDtoMapper mapper;
+
+    @Mock
+    private HttpServletRequest httpServletRequest;
 
     @InjectMocks
     private ReservationsResource reservationsResource;
@@ -41,18 +55,37 @@ class ReservationsResourceTest {
     }
 
     @Test
-    void getReservationById() {
+    void getReservationByIdShouldCallService() {
+        Long reservationId = new Random().nextLong();
+        reservationsResource.getReservationById(reservationId);
+        verify(reservationServiceMock, only()).getById(ArgumentMatchers.eq(reservationId));
     }
 
     @Test
     void createReservation() {
+        when(reservationServiceMock.save(any())).thenReturn(new Reservation());
+        when(mapper.toEntity(any())).thenReturn(new Reservation());
+        when(httpServletRequest.getRequestURL()).thenReturn(new StringBuffer(URL));
+        reservationsResource.createReservation(new ReservationDto(), httpServletRequest);
+        verify(reservationServiceMock, only()).save(any(Reservation.class));
     }
 
     @Test
-    void updateReservation() {
+    void updateReservationShouldCallServiceOnSameReservationIdInUrlAndDto() {
+        reservationsResource.updateReservation(SAME_RESERVATION_ID, ReservationDto.builder().id(SAME_RESERVATION_ID).build());
+        verify(reservationServiceMock, only()).update(any());
     }
 
     @Test
-    void deleteReservation() {
+    void updateReservationShouldThrowExceptionOnDifferentReservationIdInUrlAndDto() {
+        assertThrows(ValidationException.class,
+                () -> reservationsResource.updateReservation(SAME_RESERVATION_ID, ReservationDto.builder().id(DIFFERENT_RESERVATION_ID).build()));
+    }
+
+    @Test
+    void deleteReservationShouldCallService() {
+        Long reservationId = new Random().nextLong();
+        reservationsResource.deleteReservation(reservationId);
+        verify(reservationServiceMock, only()).delete(reservationId);
     }
 }
